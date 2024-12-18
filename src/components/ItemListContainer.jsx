8d33c94bab52db138744412a4bf0fb519b3f53fb
fabricio roberts <robertsfabricio68@gmail.com>
2024-12-18 14:23:29 -0300

    FlexContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,3 +1,29 @@
-export default function ItemListContainer({ text }) {
-  return <h2 className="greetings">{text}</h2>;
+import CardProduct from "./CardProduct";
+import FlexContainer from "./FlexContainer";
+import getAsyncData from "../data/getAsyncData";
+import { useState, useEffect } from "react";
+export default function ItemListContainer() {
+  const [products, setProducts] = useState([]);
+  console.log("render de itemlist");
+  useEffect(() => {
+    const respuestaPromise = getAsyncData();
+    console.log(respuestaPromise);
+    respuestaPromise
+      .then((respuesta) => setProducts(respuesta))
+      .catch((error) => alert(error));
+  }, []);
+  const list = products.map((prod) => (
+    <CardProduct
+      key={prod.id}
+      title={prod.title}
+      price={prod.price}
+      text={prod.category}
+      img={prod.img}
+    />
+  ));
+  return (
+    <div>
+      <FlexContainer>{list}</FlexContainer>
+    </div>
+  );
 }
